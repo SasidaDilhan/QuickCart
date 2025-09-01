0c0692daf84ae9822ddbe53c11b7a1a5d479e5d9
sasida dilhan <it21158254@my.sliit.lk>
2025-09-01 14:51:03 +0530

    order route creted

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,4 +1,6 @@
+import { inngest } from "@/config/inngest";
 import Product from "@/model/Product";
+import User from "@/model/user";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -16,9 +18,32 @@ export async function POST(request) {
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
       return acc + product.offerPrice * item.quantity;
-    },0);
+    }, 0);
 
+    await inngest.send({
+      name: "order/created",
+      data: {
+        userId,
+        address,
+        amount: amount + Math.floor(amount * 0.02),
+        date: Date.now,
+      },
+    });
 
-    
-  } catch (error) {}
+    //cleare user cart
+    const user = await User.findById(userId);
+    user.cartItems = {};
+    await user.save();
+
+    return NextResponse.json({
+      success: true,
+      message: "order placed",
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
